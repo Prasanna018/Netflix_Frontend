c9f1829118e1a897182201d4981d390f0cc122b0
Prasanna Gaikwad <prasannagaikwad0018@gmail.com>
2024-11-15 17:37:11 +0530

    link

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,6 @@
 
-export const API_END_POINT = `http://localhost:7000/api/auth/user/`; // imp
+export const API_END_POINT = "https://netflix-backend-aezn.onrender.com"
+//  `http://localhost:7000/api/auth/user/`; // imp
 export const options = {
     method: 'GET',
     headers: {
